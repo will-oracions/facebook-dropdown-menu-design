661690ffadef0214fe4073b06f10a9702d70d080
will oracions <Oracions.dev@gmail.com>
2021-11-04 16:19:01 +0100

    finalize dropdown design

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@
   const help_drop = document.querySelector(".help-drop");
   const setting_item = document.querySelector(".setting-item");
   const help_item = document.querySelector(".help-item");
+  const back_setting_btn = document.querySelector(".back-setting-btn");
+  const back_help_btn = document.querySelector(".back-help-btn");
 
   drop_btn.onclick = () => {
     menu_wrapper.classList.toggle("show");
@@ -15,12 +17,26 @@
 
   setting_item.onclick = () => {
     menu_bar.style.marginLeft = "-400px";
-    setting_drop.style.display = "block";
+    setTimeout(() => {
+      setting_drop.style.display = "block";
+    }, 100);
   };
 
   help_item.onclick = () => {
     menu_bar.style.marginLeft = "-400px";
-    help_drop.style.display = "block";
+    setTimeout(() => {
+      help_drop.style.display = "block";
+    }, 100);
+  };
+
+  back_setting_btn.onclick = () => {
+    menu_bar.style.marginLeft = "0";
+    setting_drop.style.display = "none";
+  };
+
+  back_help_btn.onclick = () => {
+    menu_bar.style.marginLeft = "0";
+    help_drop.style.display = "none";
   };
 
   // Exec
